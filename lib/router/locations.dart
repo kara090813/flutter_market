0b2a2f482be6324f6ee866f11a33026448e527fa
Project_yun <kara090813@gmail.com>
2022-02-20 20:47:31 +0900

    auth page success

---
+++ lib/router/locations.dart
@@ -0,0 +1,26 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/widgets.dart';
+import 'package:market/screens/auth_screen.dart';
+import 'package:market/screens/home_screen.dart';
+
+class HomeLocation extends BeamLocation{
+  @override
+  List<BeamPage> buildPages(BuildContext context, RouteInformationSerializable state) {
+    return [BeamPage(child: HomeScreen(), key:ValueKey('home'))];
+  }
+
+  @override
+  List<Pattern> get pathPatterns => ['/'];
+
+}
+
+class AuthLocation extends BeamLocation{
+  @override
+  List<BeamPage> buildPages(BuildContext context, RouteInformationSerializable state) {
+    return [BeamPage(child: AuthScreen(), key:ValueKey('auth'))];
+  }
+
+  @override
+  List<Pattern> get pathPatterns => ['/auth'];
+
+}
